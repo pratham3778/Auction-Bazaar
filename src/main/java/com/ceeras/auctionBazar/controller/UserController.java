7a979cfbea4a6faafeca1dddc25453a1fe051bf8
Chaithra-Prakash <139253580+Chaithra-Prakash@users.noreply.github.com>
2025-02-19 16:18:46 +0400

    Create UserController.java

---
+++ src/main/java/com/ceeras/auctionBazar/controller/UserController.java
@@ -0,0 +1,22 @@
+package in.ceeras.RegistrationAPI.controller;
+
+import in.ceeras.RegistrationAPI.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private UserService userService;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody Map<String, String> userData) {
+        String email = userData.get("email");
+        String password = userData.get("password");
+        String name = userData.get("name");
+
+        return userService.registerUser(email, password, name);
+    }
+}
