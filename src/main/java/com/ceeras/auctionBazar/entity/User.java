3b116f5b7cc5d46e4c797beab506f16ca613fb21
Karok365 <anshsinghh365@gmail.com>
2025-02-19 11:58:01 +0530

    general structure

---
+++ src/main/java/com/ceeras/auctionBazar/entity/User.java
@@ -0,0 +1,27 @@
+package com.ceeras.auctionBazar.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+}
