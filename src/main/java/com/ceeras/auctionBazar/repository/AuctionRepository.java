ad894992eb198fb9a053dfb3a12e7b3341098f73
Chaithra-Prakash <139253580+Chaithra-Prakash@users.noreply.github.com>
2025-05-22 12:14:29 +0400

    Delete src/main/java/com/ceeras/auctionBazar directory

--- src/main/java/com/ceeras/auctionBazar/repository/AuctionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ceeras.auctionBazar.repository;
-
-import com.ceeras.auctionBazar.entity.Auction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface AuctionRepository extends JpaRepository<Auction, Long> {
-    List<Auction> findByCreatorId(Long creatorId);
-}
